1077cbc96205876dbe0a3c6a15c90fa0be88b578
jeff-k <jeff@x-0.ca>
2018-08-09 17:01:31 -0700

    relax thresholds for 2x250 reads

--- src/main.rs
+++ src/main.rs
@@ -94,14 +94,14 @@ fn merge_records(r1: &Record, r2: &Record) -> Option<Record> {
     let r2_rc = dna::revcomp(r2.seq());
     let r1_rc = dna::revcomp(r1.seq());
 
-    match mate(&r1.seq(), &r2_rc, 25, 25) {
+    match mate(&r1.seq(), &r2_rc, 25, 20) {
         Some(overlap) => {
             let seq = merge(&r1.seq(), &r2_rc, overlap);
             let qual = merge(&r1.qual(), &r2.qual(), overlap);
             Some(Record::with_attrs(r1.id(), None, &seq, &qual))
         },
         None => {
-            match mate(&r1_rc, &r2.seq(), 25, 25) {
+            match mate(&r1_rc, &r2.seq(), 25, 20) {
                 Some(overlap) => {
                     let seq = truncate(&r1.seq(), &r2_rc, overlap);
                     let qual = truncate(&r1.qual(), &r2.qual(), overlap);
