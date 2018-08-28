1ad7ae44d31e37d81ef742f8a6a45a20bee6d763
jeff-k <jeff@x-0.ca>
2018-08-28 16:46:55 -0700

    support writing out unmerged reads (WIP)

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,54 @@ extern crate bio;
 extern crate clap;
 
 use bio::alphabets::dna;
-use bio::io::fastq::{Reader, Writer, Record};
+use bio::io::fastq::{Reader, Record, Writer};
 use bio::pattern_matching::mating::{mate, merge, truncate};
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 use std::fs::File;
 
-use std::path::Path;
 use std::io::{self, Write};
+use std::path::Path;
 
 fn main() {
     let args = App::new("merge-reads")
         .version("0.1")
         .about("Merge paired Illumina reads")
         .author("jeff-k <jeff_k@fastmail.com>")
-        .arg(Arg::with_name("R1")
-             .help("Forward read")
-             .required(true)
-             .index(1))
-        .arg(Arg::with_name("R2")
-             .help("Reverse read")
-             .required(true)
-             .index(2))
-        .arg(Arg::with_name("out")
-             .short("o")
-             .long("out")
-             .value_name("FILE")
-             .help("Write to fastq file instead of STDOUT")
-             .takes_value(true))
-        .arg(Arg::with_name("prefix")
-             .short("p")
-             .long("prefix")
-             .value_name("PREFIX")
-             .help("Write unmerged reads to paired fastq files at PREFIX-Rx.fq"))
-        .arg(Arg::with_name("stats")
-             .short("s")
-             .long("stats")
-             .help("Print merge statistics to STDERR when done"))
+        .arg(
+            Arg::with_name("R1")
+                .help("Forward read")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("R2")
+                .help("Reverse read")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("out")
+                .short("o")
+                .long("out")
+                .value_name("FILE")
+                .help("Write to fastq file instead of STDOUT")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("prefix")
+                .short("p")
+                .long("prefix")
+                .value_name("PREFIX")
+                .help("Write unmerged reads to paired fastq files at PREFIX-Rx.fq"),
+        )
+        .arg(
+            Arg::with_name("stats")
+                .short("s")
+                .long("stats")
+                .help("Print merge statistics to STDERR when done"),
+        )
         .get_matches();
 
     let r1_path = args.value_of("R1").unwrap();
@@ -52,8 +62,20 @@ fn main() {
         Some(fp) => {
             let path = Path::new(fp);
             Box::new(File::create(&path).unwrap()) as Box<Write>
-        },
+        }
         None => Box::new(io::stdout()), // output to STDOUT
+    };
+
+    let unmerged_handles: Option<(Write, Write)> = match args.value_of("prefix") {
+        Some(fp) => {
+            let path_f = Path::new(fp + ".unmerged.fq");
+            let path_r = Path::new(fp + ".unmerged.fq");
+            Some((
+                File::create(&path_f).unwrap(),
+                File::create(&path_r).unwrap(),
+            )) as Option<(Write, Write)>
+        }
+        None => None,
     };
 
     let mates1 = Reader::new(r1).records();
@@ -61,31 +83,31 @@ fn main() {
 
     let mut merged = Writer::new(out_handle);
 
+    //    let mut unmerged_f = Writer::new(unmerged_handle_f);
+    //    let mut unmerged_r = Writer::new(unmerged_handle_r);
+
     let mut lengths = vec![0; 502];
     let mut total_frags: usize = 0;
     let mut total_merged = 0;
     for (r1, r2) in mates1.zip(mates2) {
         total_frags += 1;
         match (r1, r2) {
-            (Ok(x), Ok(y)) => {
-                match merge_records(&x, &y) {
-                    Some(r) => {
-                        lengths[r.seq().len()] += 1;
-                        merged.write_record(&r).unwrap();
-                        total_merged += 1;
-                    },
-                    None => {
-                        ()
-                    },
+            (Ok(x), Ok(y)) => match merge_records(&x, &y) {
+                Some(r) => {
+                    lengths[r.seq().len()] += 1;
+                    merged.write_record(&r).unwrap();
+                    total_merged += 1;
                 }
+                None => (),
             },
             _ => eprintln!("unsynced fastqs"),
         }
-    } 
+    }
     if args.is_present("stats") {
-        eprintln!("Total reads:\t{0}\nMerged reads\t{1}",
-                  total_frags,
-                  total_merged);
+        eprintln!(
+            "Total reads:\t{0}\nMerged reads\t{1}",
+            total_frags, total_merged
+        );
         eprintln!("{:?}", lengths);
     }
 }
@@ -99,16 +121,14 @@ fn merge_records(r1: &Record, r2: &Record) -> Option<Record> {
             let seq = merge(&r1.seq(), &r2_rc, overlap);
             let qual = merge(&r1.qual(), &r2.qual(), overlap);
             Some(Record::with_attrs(r1.id(), None, &seq, &qual))
-        },
-        None => {
-            match mate(&r1_rc, &r2.seq(), 25, 20) {
-                Some(overlap) => {
-                    let seq = truncate(&r1.seq(), &r2_rc, overlap);
-                    let qual = truncate(&r1.qual(), &r2.qual(), overlap);
-                    Some(Record::with_attrs(r1.id(), None, &seq, &qual))
-                }
-                None => None,
+        }
+        None => match mate(&r1_rc, &r2.seq(), 25, 20) {
+            Some(overlap) => {
+                let seq = truncate(&r1.seq(), &r2_rc, overlap);
+                let qual = truncate(&r1.qual(), &r2.qual(), overlap);
+                Some(Record::with_attrs(r1.id(), None, &seq, &qual))
             }
+            None => None,
         },
     }
 }
